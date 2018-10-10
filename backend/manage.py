2277bc420a0e425b7308a01458779e3a06492a7b
Panos Tzimos <panos.tzimos@therealbuzzgroup.com>
2018-10-10 19:30:20 +0100

    remove .pyc files before running the backend server

--- backend/manage.py
+++ backend/manage.py
@@ -5,6 +5,9 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.local')
     try:
+        # We have to delete the .pyc files before we run the project,
+        # in order to avoid to execute cached code.
+        os.system('find . -name "*.pyc" -exec rm -f {} \;')
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
